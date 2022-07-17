cef42d392278eccb9ede3938fb3c1f06369dedb1
Alfin7007 <alfin.7007@gmail.com>
2022-07-17 13:17:53 -0400

    repair response update user,add send email

--- config/app.go
+++ config/app.go
@@ -12,8 +12,4 @@ func JWT() string {
 const Admin = "admin"
 const User = "user"
 const UMKM = "umkm"
-<<<<<<< HEAD
-
-=======
 const Status = "waiting"
->>>>>>> f06af611cbbd14d15f271ff4f64aef978094944d
